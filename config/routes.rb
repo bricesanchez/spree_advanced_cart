bb73004a8234eae66cb32bf9922906f5069ece57
Brice Sanchez <contact@brice-sanchez.com>
2013-11-14 22:32:40 -0500

    Update route estimate_shipping_cost to fix spree 2.1.x error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
-Spree::Core::Engine.routes.append do
-
-  get "/estimate_shipping_cost", :to => 'orders#estimate_shipping_cost', :as => :estimate_shipping_cost
-
+Spree::Core::Engine.routes.draw do
+  get 'estimate_shipping_cost' => 'orders#estimate_shipping_cost', :as => :estimate_shipping_cost
 end
 
